d669fb63739080fb76b76c4f37a8f1440b3e0a2c
rdireen <rdireen@direentech.com>
2015-03-08 14:49:18 -0600

    Initial commit into git.

---
+++ TestLibrary/Calculate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Calculator
+{
+	public class Calculate
+	{
+		public int x;
+		public int y;
+
+		public Calculate (int x, int y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+
+		public int add()
+		{
+			return x + y;
+		}
+
+		public int sub()
+		{
+			return x - y;
+		}
+
+		public int mult()
+		{
+			return x * y;
+		}
+
+		public int div()
+		{
+			return x / y;
+		}
+
+	}
+}
+
---
+++ simple/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Calculator;
+
+namespace simple
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int x = 0;
+			int y = 0;
+			int r = 0;
+
+			Console.WriteLine ("Here is a calculator");
+			Console.WriteLine ("Enter x: ");
+			var line_x = Console.ReadLine ();
+			Console.WriteLine ("Enter x: ");
+			var line_y = Console.ReadLine ();
+
+			if (int.TryParse (line_x, out x) && int.TryParse (line_y, out y)) {
+
+				var calc = new Calculate (x, y);
+
+				Console.WriteLine ("");
+				Console.WriteLine ("Enter 1) add; 2) sub; 3) mult; 4) div: ");
+				var calc_type = Console.ReadLine ();
+
+				switch (calc_type) {
+
+				case "1":
+					r = calc.add ();
+					break;
+				case "2":
+					r = calc.sub ();
+					break;
+				case "3":
+					r = calc.mult ();
+					break;
+				case "4":
+					r = calc.div ();
+					break;
+				}
+
+				Console.WriteLine (String.Format("Your result is {0}", r)); 
+
+
+
+			} else {
+				Console.WriteLine ("Enter integers only.");
+			}
+
+			Console.WriteLine ("*** THE END ***");
+
+		}
+	}
+}
